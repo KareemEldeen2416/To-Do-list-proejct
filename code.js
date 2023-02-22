e076f12ce3c6f58ec0912c47e6d3098b4659c1dc
KareemEldeen2416 <120192930+KareemEldeen2416@users.noreply.github.com>
2023-02-22 19:40:43 +0200

    Action to add list and note is added

--- code.js
+++ code.js
@@ -1,15 +1,43 @@
 
 
 const documentFragment = document.createDocumentFragment();
-const listArea = document.querySelector('.collection');
+const listArea = document.getElementById('workArea');
 const newNoteButton = document.getElementById('newNote');
+const newListButton = document.getElementById('newList');
 
-function addNote(){
+
+
+
+//Action to add a new note
+newNoteButton.addEventListener("click" , ()=>{
+    console.log("done");
     const newNote = document.createElement('div');
-    newNote.classList.add("note");
+    newNote.setAttribute('class' , 'note');
+    const noteTitle = document.createElement("h3");
+    noteTitle.setAttribute('class' , 'noteTitle');
+    noteTitle.textContent = "Note";
+    newNote.appendChild(noteTitle);
+    const breaker = document.createElement('br');
+    newNote.appendChild(breaker);
+    const noteContent = document.createElement('textarea');
+    noteContent.setAttribute('placeholder' , 'Your note here');
+    newNote.appendChild(noteContent);
     documentFragment.appendChild(newNote);
     listArea.appendChild(documentFragment);
-}
+});
 
 
-newNoteButton.addEventListener('click' , addNote);
+//Action to add a new list
+newListButton.addEventListener('click', ()=>{
+    console.log('new list added');
+    const newList = document.createElement('div');
+    newList.setAttribute('class' ,'note');
+    const listTitle = document.createElement('h3');
+    listTitle.textContent="To-Do";
+    listTitle.setAttribute('class','listTitle');
+    newList.appendChild(listTitle);
+    const breaker = document.createElement('br');
+    newList.appendChild(breaker);
+    documentFragment.appendChild(newList);
+    listArea.appendChild(documentFragment);
+});
