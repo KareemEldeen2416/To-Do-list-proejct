19d6f4e0e997c830e1391c8dccce8324f9112b6e
KareemEldeen2416 <120192930+KareemEldeen2416@users.noreply.github.com>
2023-03-04 18:47:33 +0200

    After modifications

--- code.js
+++ code.js
@@ -4,6 +4,8 @@ const documentFragment = document.createDocumentFragment();
 const listArea = document.getElementById('workArea');
 const newNoteButton = document.getElementById('newNote');
 const newListButton = document.getElementById('newList');
+let listsID = [1];
+let notesID = [1];
 
 
 
@@ -13,9 +15,9 @@ newNoteButton.addEventListener("click" , ()=>{
     console.log("done");
     const newNote = document.createElement('div');
     newNote.setAttribute('class' , 'note');
-    const noteTitle = document.createElement("h3");
+    const noteTitle = document.createElement("input");
     noteTitle.setAttribute('class' , 'noteTitle');
-    noteTitle.textContent = "Note";
+    noteTitle.setAttribute('placeholder' , "Enter note title here");
     newNote.appendChild(noteTitle);
     const breaker = document.createElement('br');
     newNote.appendChild(breaker);
@@ -29,32 +31,94 @@ newNoteButton.addEventListener("click" , ()=>{
 
 //Action to add a new list
 newListButton.addEventListener('click', ()=>{
+    const newID = listsID[listsID.length-1] + 1;
+    listsID.push(newID);
+    let colors  = ['yellow' , 'blue' , 'purple' , 'pink' , 'grey'];
+    let colorsValues = ['#fdf3b1','#8ecdef' ,'rgba(189, 130, 244, 0.83)','#fdbef6','#dadadb' ];
     console.log('new list added');
+    //create the body of the list
     const newList = document.createElement('div');
-    newList.setAttribute('class' ,'note');
-    const listTitle = document.createElement('h3');
-    listTitle.textContent="To-Do";
-    listTitle.setAttribute('class','listTitle');
+    newList.setAttribute('class' , 'note');
+    newList.setAttribute('id' , newID);
+    //create title of the list
+    const listTitle = document.createElement('input');
+    listTitle.setAttribute('type' , 'text');
+    listTitle.setAttribute('class' , 'noteTitle');
+    listTitle.setAttribute('placeholder' , 'list title');
+
+    //create add button
+    const addButton = document.createElement('button');
+    addButton.setAttribute('class' , 'addNote');
+    addButton.textContent = '+';
+    addButton.addEventListener('click' , ()=>{
+        const selectedList = document.getElementsByClassName(newID);
+        
+        //create a new task
+        const newTask = document.createElement('li');
+        const newTaskBody = document.createElement('div');
+        const newCheckBox = document.createElement('input');
+        newCheckBox.setAttribute('type' , 'checkbox');
+        newTaskBody.appendChild(newCheckBox);
+        const newTaskTitle = document.createElement('input');
+        newTaskTitle.setAttribute('type' , 'text');
+        newTaskTitle.setAttribute('class' , 'noteContent');
+        newTaskTitle.setAttribute('placeholder' , 'Task here')
+        newTaskBody.appendChild(newTaskTitle);
+        newTask.appendChild(newTaskBody);
+        selectedList[0].appendChild(newTask);
+
+        
+    });
+
+    //appending list title, and add button to the body of the list
     newList.appendChild(listTitle);
-    const toDo = document.createElement('div');
-    const checkList = document.createElement('ul');
-    checkList.setAttribute('class' , 'checkList');
-    const listItem = document.createElement('li');
-    const itemContent = document.createElement('div');
-    const check = document.createElement('input');
-    check.setAttribute('type' , 'checkbox');
-    const checkLabel = document.createElement('input');
-    checkLabel.setAttribute('placeholder' , 'Task here');
-    checkLabel.setAttribute('type' , 'text');
-    checkLabel.setAttribute('class' , 'noteContent');
-    itemContent.appendChild(check);
-    itemContent.appendChild(checkLabel);
-    listItem.appendChild(itemContent);
-    checkList.appendChild(listItem);
-    toDo.appendChild(checkList);
+    newList.appendChild(addButton);
+
+    //create break, and adding it to the body
     const breaker = document.createElement('br');
     newList.appendChild(breaker);
-    newList.appendChild(toDo);
+
+    //create body of color list
+    const colorList = document.createElement('div');
+    colorList.setAttribute('class' , 'colorList');
+
+    //adding colors to the body of color list
+    for(let i = 0 ; i < colors.length ; i++){
+        const newColor = document.createElement('button');
+        newColor.setAttribute('class' , 'box '+colors[i]);
+        //Add event to change color
+        newColor.addEventListener('click' , ()=>{
+            document.getElementById(newID).style.backgroundColor = colorsValues[i];
+        });
+        colorList.appendChild(newColor);
+    }
+
+    //adding color list to the body of the list
+    newList.appendChild(colorList);
+
+    //create task list
+    const taskList = document.createElement('ul');
+    taskList.setAttribute('class' , 'checkList '+newID);
+    
+
+    //create an initial task
+    const task = document.createElement('li');
+    const taskbody = document.createElement('div');
+    const check = document.createElement('input');
+    check.setAttribute('type' , 'checkbox');
+    const taskTitle = document.createElement('input');
+    taskTitle.setAttribute('type' , 'text');
+    taskTitle.setAttribute('class' , 'noteContent');
+    taskTitle.setAttribute('placeholder' , 'Task here')
+    taskbody.appendChild(check);
+    taskbody.appendChild(taskTitle);
+    task.appendChild(taskbody);
+    taskList.appendChild(task);
+
+    //append task list to the body of the list
+    newList.appendChild(taskList);
+    
+
     documentFragment.appendChild(newList);
     listArea.appendChild(documentFragment);
 });
